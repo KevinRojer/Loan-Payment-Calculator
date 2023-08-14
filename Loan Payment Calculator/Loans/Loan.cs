166b98c4defb1897a6e6c871ad9e201f8b3fc750
Kevin Rojer <kevin.rojer@live.com>
2023-08-14 17:34:59 +0200

    Changed location

---
+++ Loan Payment Calculator/Loans/Loan.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LoanPaymentCalculator
+{
+	public class Loan
+	{
+		private double Principal;
+		private double AnnualRate;
+		private int NumPeriods;
+
+		public Loan(double amount, double intRate, int term)
+		{
+			this.Principal = amount;
+			this.AnnualRate = intRate / 100;
+			this.NumPeriods = term;
+		}
+
+		public double CalculateLoanPayment()
+		{
+			// Calculates monthly payment for Amortization Loans
+			double monthlyRate = AnnualRate / 12;
+			double monthlyPayment = (Principal * monthlyRate * Math.Pow(1 + monthlyRate, NumPeriods)) / (Math.Pow(1 + monthlyRate, NumPeriods) - 1);
+			return monthlyPayment;
+		}
+	}
+}
+
---
+++ Loan Payment Calculator/Loans/LoanManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LoanPaymentCalculator
+{
+	public class LoanManager
+	{
+		private List<Loan> loans;
+
+		public LoanManager()
+		{
+			loans = new List<Loan>();
+		}
+
+		public void AddLoan(Loan loan)
+		{
+			loans.Add(loan);
+		}
+
+		public void CompareLoans()
+		{
+			Console.WriteLine();
+			Console.WriteLine("Loan Comparisson Results:");
+			Console.WriteLine("-------------------------------");
+            Console.WriteLine();
+			Console.WriteLine("The monthly payment for the loans are:");
+			Console.WriteLine();
+
+
+            int counter = 1;
+			foreach (Loan loan in loans)
+			{
+				double monthlyPayment = loan.CalculateLoanPayment();
+				Console.WriteLine($" {counter}. €{monthlyPayment:0.00}");
+				counter++;
+			}
+
+			Console.WriteLine();
+		}
+	}
+}
+
